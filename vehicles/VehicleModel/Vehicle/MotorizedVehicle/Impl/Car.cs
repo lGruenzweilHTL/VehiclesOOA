ec88b23862f4e02ad011f037c510e48884785543
Integr-0 <erik.reitbauer@outlook.de>
2025-02-25 14:17:37 +0100

    Changes

---
+++ vehicles/VehicleModel/Vehicle/MotorizedVehicle/Impl/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace VehicleModel;
+
+public class Car : MotorizedVehicle
+{
+    public override double Speed { get; protected set; }
+    public override (double consumption, TimeSpan time) Drive(double distanceKilometer, DrivingMode drivingMode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override double BaseConsumptionPerKilometer { get; protected set; }
+    public override int FuelCap { get; protected set; }
+    public override int FuelLevel { get; protected set; }
+    public override FuelType FuelType { get; protected set; }
+}
