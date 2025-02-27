c7507530228a29eace59730e0c26b0bae1bac26d
Integr-0 <erik.reitbauer@outlook.de>
2025-02-27 10:38:43 +0100

    Added BicycleTests.cs

---
+++ vehicles/VehicleModelTests/BicycleTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using VehicleModel;
+using Xunit;
+
+namespace VehicleModelTests;
+
+public class BicycleTests
+{
+    [Theory]
+    [InlineData(100, 100, RoadType.CityRoad)]
+    [InlineData(100, 100, RoadType.Highway)]
+    [InlineData(50, 60, RoadType.Offroad)]
+    [InlineData(50, 60, RoadType.Backroad)]
+    [InlineData(0, 0, RoadType.CityRoad)]
+    [InlineData(27, 2, RoadType.Highway)]
+    public void Drive_GivenConstructionAndDrivingParams_ReturnsCorrectTime(double distanceKilometer, double baseSpeed, RoadType roadType)
+    {
+        var bicycle = TestingHelper.VehicleBuilder.Start()
+            .WithSpeed(baseSpeed)
+            .BuildBicycle();
+        
+        var (consumption, time) = bicycle.Drive(distanceKilometer, roadType);
+
+        var expectedTimeHours = baseSpeed == 0 ? 0 : distanceKilometer / (baseSpeed * TestingHelper.Constants.SpeedMultiplier.GetSpeedMultiplier(roadType));        
+        
+        time.Should().Be(TimeSpan.FromHours(expectedTimeHours), "the time of the drive should be equal to the expected time.");
+        consumption.Should().Be(0, "the consumption of a bicycle should always be 0.");
+    }
+}
